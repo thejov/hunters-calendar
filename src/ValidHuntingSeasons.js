49a2cb315b09304c7f624a8cc53e822a61e578eb
Juha Viljanen <juha.o.viljanen@gmail.com>
2018-01-27 00:24:48 +0200

    Add ValidHuntingSeasons component
    This component shows a card for each passed animal's hunting season

---
+++ src/ValidHuntingSeasons.js
@@ -0,0 +1,32 @@
+// @flow
+
+import React from "react";
+import { Well } from "react-bootstrap";
+import "./ValidHuntingSeasons.css";
+
+type HuntingSeasonCardsProps = {
+  huntingSeasons: Array<Object>
+};
+
+export const ValidHuntingSeasons = (props: HuntingSeasonCardsProps) =>
+  props.huntingSeasons.map(animal => (
+    <AnimalCard
+      species={animal.species}
+      region={animal.region}
+      huntingSeason={animal.huntingSeason}
+    />
+  ));
+
+type AnimalCardProps = {
+  species: string,
+  region: string,
+  huntingSeason: string
+};
+
+const AnimalCard = (props: AnimalCardProps) => (
+  <Well className="animalCard" bsSize="large">
+    <h2>{props.species}</h2>
+    <p>{props.region}</p>
+    <p>{props.huntingSeason}</p>
+  </Well>
+);
