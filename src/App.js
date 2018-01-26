ecf409a4cb0c2dd56b0d3f2a59f8069d82db970c
Juha Viljanen <juha.o.viljanen@gmail.com>
2018-01-27 00:36:52 +0200

    Change React logo to shotgun image

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 // @flow
 
 import React, { Component } from "react";
-import logo from "./logo.svg";
+
+// Foto Joachim Bäcker 2005/09/25 under CC BY-SA 3.0 license
+// https://creativecommons.org/licenses/by-sa/3.0/
+// Modifications done: Removed white background, changed filename
+import shotgun from "./shotgun.png";
 import "./App.css";
 import { huntingSeasons } from "./helpers/fixtures/complete_parsed_hunting_seasons";
 import { ValidHuntingSeasons } from "./ValidHuntingSeasons";
@@ -11,7 +15,7 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+          <img src={shotgun} className="App-logo" alt="shotgun" />
           <h1 className="App-title">Mitä tänään saa metsästää?</h1>
         </header>
         <ValidHuntingSeasons huntingSeasons={huntingSeasons} />
