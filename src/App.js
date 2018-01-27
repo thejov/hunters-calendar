ef0b1a316db970190f83a52501b09f341bb2a778
Juha Viljanen <juha.o.viljanen@gmail.com>
2018-01-27 15:21:22 +0200

    Change favicon

--- src/App.js
+++ src/App.js
@@ -17,6 +17,39 @@ const App = () => (
       <h1 className="App-title">Mitä tänään saa metsästää?</h1>
     </header>
     <ValidHuntingSeasons huntingSeasons={huntingSeasons} today={new Date()} />
+    <IconCredits />
+  </div>
+);
+
+const IconCredits = () => (
+  <div className="iconCredits">
+    Icons made by{" "}
+    <a
+      href="http://www.freepik.com"
+      target="_blank"
+      rel="noopener noreferrer"
+      title="Freepik"
+    >
+      Freepik
+    </a>{" "}
+    from{" "}
+    <a
+      href="https://www.flaticon.com/"
+      target="_blank"
+      rel="noopener noreferrer"
+      title="Flaticon"
+    >
+      www.flaticon.com
+    </a>{" "}
+    is licensed by{" "}
+    <a
+      href="http://creativecommons.org/licenses/by/3.0/"
+      title="Creative Commons BY 3.0"
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      CC 3.0 BY
+    </a>
   </div>
 );
 
