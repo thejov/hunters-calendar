63892ab20ed0e486715f34ee5420d099bfb50ed0
Juha Viljanen <juha.o.viljanen@gmail.com>
2018-01-27 14:33:52 +0200

    Make App class to const

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 // @flow
 
-import React, { Component } from "react";
+import React from "react";
 
 // Foto Joachim Bäcker 2005/09/25 under CC BY-SA 3.0 license
 // https://creativecommons.org/licenses/by-sa/3.0/
@@ -10,18 +10,14 @@ import "./App.css";
 import { huntingSeasons } from "./helpers/fixtures/hunting_seasons";
 import { ValidHuntingSeasons } from "./ValidHuntingSeasons";
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={shotgun} className="App-logo" alt="shotgun" />
-          <h1 className="App-title">Mitä tänään saa metsästää?</h1>
-        </header>
-        <ValidHuntingSeasons huntingSeasons={huntingSeasons} />
-      </div>
-    );
-  }
-}
+const App = () => (
+  <div className="App">
+    <header className="App-header">
+      <img src={shotgun} className="App-logo" alt="shotgun" />
+      <h1 className="App-title">Mitä tänään saa metsästää?</h1>
+    </header>
+    <ValidHuntingSeasons huntingSeasons={huntingSeasons} />
+  </div>
+);
 
 export default App;
